f19940c791630ac476b310b9eabe55d6c8be220a
Saad Shafiq <>
2023-03-24 12:41:18 +0100

    Postman collection and API

--- main.py
+++ main.py
@@ -23,27 +23,35 @@ async def shutdown_event():
 
 
 @app.post("/collection")
-async def create_user(user: dict):
-    created_user = await prisma.user.create(user)
+async def create_user(collection: dict):
+    created_user = await prisma.collection.create(collection)
     return created_user
 
 
-@app.get("/collection/{user_id}")
-async def get_user(user_id: int):
-    user = await prisma.user.find_unique(where={"id": user_id})
+@app.get("/collection/{id}")
+async def get_user(id):
+    user = await prisma.collection.find_unique(where={"id": id})
     return user
 
 
-@app.put("/collection/{user_id}")
-async def update_user(user_id: int, user_data: dict):
-    updated_user = await prisma.user.update(where={"id": user_id}, data=user_data)
+@app.put("/collection/{id}")
+async def update_user(id: str, collection_data: dict):
+    updated_user = await prisma.collection.update(
+        where={"id": id}, data=collection_data
+    )
     return updated_user
 
 
-@app.delete("/collection/{user_id}")
-async def delete_user(user_id: int):
-    deleted_user = await prisma.user.delete(where={"id": user_id})
+@app.delete("/collection/{id}")
+async def delete_user(id):
+    deleted_user = await prisma.collection.delete(where={"id": id})
     return deleted_user
+
+
+@app.get("/collection/path/{language}")
+async def get_local_paths(language: str):
+    records = await prisma.collection.find_many(where={"Language": language})
+    return records
 
 
 if __name__ == "__main__":
